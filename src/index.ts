c22aaede518767656cc6b74c059fb3e44a38b724
Alexandre BERTRAND <alexandre@hublot.io>
2025-04-17 15:18:07 +0200

    fix erorr on watch

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,72 @@
 import { Ref, ref, watch } from 'vue'
 import Vue from 'vue'
-import { getCurrentInstance } from 'vue' 
+import { getCurrentInstance } from 'vue'
 
 
 const bus = new Vue()
 
 Vue.mixin({
-    props : ["value"],
+    props: ["value"],
 })
 
 
 Vue.directive('models', {
-    update: function(el, binding, vnode){
-        bus.$emit('directive-update-' + binding.arg, {el, binding, vnode} )        
+    update: function (el, binding, vnode) {
+        bus.$emit('directive-update-' + binding.arg, { el, binding, vnode })
     },
     inserted: function (el, binding, vnode) {
-        bus.$emit('directive-insert-' + binding.arg, {el, binding, vnode} ) 
+        bus.$emit('directive-insert-' + binding.arg, { el, binding, vnode })
     }
 })
 
 
 // by convention, composable function names start with "use"
-export function defineModel<T>(modelValue : string = 'input') : Ref<T>{
+export function defineModel<T>(modelValue: string = 'input'): Ref<T> {
 
     const __instance = getCurrentInstance().proxy;
     const __internalValue = ref<T>()
 
-    if(modelValue === 'input'){
+    if (modelValue === 'input') {
         __internalValue.value = __instance.$props.value as T
 
-        setTimeout(() => {
-            watch(__instance.$props, () => {
+        watch(__instance.$props, () => {
+            setTimeout(() => {
                 __internalValue.value = __instance.$props.value as T
-            }, { deep : true})
+            })
+        }, { deep: true })
 
-        })
     }
     else {
-        bus.$on('directive-insert-' + modelValue, ({el, binding, vnode}) => {
-            if(binding.value)
+        bus.$on('directive-insert-' + modelValue, ({ el, binding, vnode }) => {
+            if (binding.value)
                 __internalValue.value = binding.value
-           
+
             setTimeout(() => {
-                 bus.$emit(vnode.context!.$vnode.tag! + '-' + binding.arg,  __internalValue.value);
+                bus.$emit(vnode.context!.$vnode.tag! + '-' + binding.arg, __internalValue.value);
             })
 
             watch(__internalValue, () => {
-                if(__internalValue.value !== binding.value)
-                    bus.$emit(vnode.context!.$vnode.tag! + '-' + binding.arg,  __internalValue.value);
-            }, { deep : true})
+                if (__internalValue.value !== binding.value)
+                    bus.$emit(vnode.context!.$vnode.tag! + '-' + binding.arg, __internalValue.value);
+            }, { deep: true })
         })
 
-        bus.$on('directive-update-' + modelValue, ({el, binding, vnode}) => { 
+        bus.$on('directive-update-' + modelValue, ({ el, binding, vnode }) => {
             __internalValue.value = binding.value
         })
     }
 
-   
+
     watch(__internalValue, () => {
-         if(modelValue === 'input')
-             __instance.$emit(modelValue, __internalValue.value);
-    }, { deep : true})
-  
+        if (modelValue === 'input')
+            __instance.$emit(modelValue, __internalValue.value);
+    }, { deep: true })
+
     return __internalValue as Ref<T>
 
 }
 
-export const bindModels = function(models){
+export const bindModels = function (models) {
     const modelsNameList = Object.getOwnPropertyNames(models)
     setTimeout(() => {
         modelsNameList.forEach((emitName) => {
